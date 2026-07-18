acd30ed101e38e51fd27d417634bae957a7487b1
Dmitrii <dimalazarevne@gmail.com>
2026-07-19 02:59:09 +0300

    add .env

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,10 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-dev-key')
     from .app import bp
-
     app.register_blueprint(bp)
     return app
 
